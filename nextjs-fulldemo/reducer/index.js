44b21cd9c5d7189b2704e1a47489635f8c12dcc8
rdwrcode <rdwrcode@gmail.com>
2017-06-17 09:41:13 -0600

    update

--- nextjs-fulldemo/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import newsListing from './newsListing'
-
-export default combineReducers({
-  newsListing
-})
--- nextjs-fulldemo/utils/index.js
+++
@@ -1,5 +0,0 @@
-export function getPagePathname(pathname, nextExport) {
-  if (!nextExport) return pathname;
-  if (pathname === '/') return '/index.js';
-  return `${pathname}/index.js`;
-}
